3d41a19755e6b97cc47097c22db41c7c09d8dd19
vishnusingh3210 <ucc.vishnusingh3210@gmail.com>
2026-01-09 05:41:37 +0530

    Implement service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,14 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("calc-cache").then(cache => {
+      return cache.addAll(["./", "./index.html"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
